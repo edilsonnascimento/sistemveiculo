20477212a5157d06d9c6a127aa7d499f12de279b
Edilson do Nascimento <edilson.nascimento@gmail.com>
2020-10-31 15:56:41 -0300

    Criado Menu Principal.

--- src/sistemveiculo/Teste.java
+++ src/sistemveiculo/Teste.java
@@ -3,6 +3,15 @@ package sistemveiculo;
 public class Teste {
 
 	public static void main(String[] args) {
-		
+
+		System.out.println("Sistema de Gestão de Veículos - Menu Inicial");
+		System.out.println("1) Cadastrar Veículo de Passeio");
+		System.out.println("2) Cadastrar Veículo de Carga");
+		System.out.println("3) Imprimir Todos os Veículos de Passeio");
+		System.out.println("4) Imprimir Todos os Veículos de Carga");
+		System.out.println("5) Imprimir Veículo de Passeio pela Placa ");
+		System.out.println("6) Imprimir Veículo de Carga pela Placa");
+		System.out.println("7) Sair do Sistema");
+
 	}
 }
